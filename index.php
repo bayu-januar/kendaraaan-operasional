941745c712ac8700818f4066342c93bf08c1e0f7
bayu-januar <bayu.januar.if@gmail.com>
2024-10-22 21:48:39 +0700

    index.php

--- index.php
+++ index.php
@@ -28,37 +28,72 @@
 <body>
     <div class="container">
         <div class="form-container">
-            <h2 class="mt-4 mb-4 text-center">Form Penggunaan Mobil</h2>
+            <h2 class="mt-4 mb-4 text-center">Form Penggunaan Kendaraan Operasional</h2>
             <form action="save_data.php" method="POST" class="needs-validation" novalidate>
                 <div class="form-group mb-3">
-                    <label for="nama">Nama Pemakai:</label>
+                    <label for="nama">Nama Peminjam:</label>
                     <input type="text" id="nama" name="nama" class="form-control" required>
                     <div class="invalid-feedback">
                         Nama harus diisi.
                     </div>
                 </div>
                 <div class="form-group mb-3">
-                    <label for="jabatan">Jabatan Pemakai:</label>
+                    <label for="jabatan">Jabatan Peminjam:</label>
                     <input type="text" id="jabatan" name="jabatan" class="form-control" required>
                     <div class="invalid-feedback">
                         Jabatan harus diisi.
                     </div>
                 </div>
                 <div class="form-group mb-3">
-                    <label for="tanggal">Tanggal Pemakaian:</label>
-                    <input type="date" id="tanggal" name="tanggal" class="form-control" required>
+                    <label for="jenis_kendaraan">Jenis Kendaraan Operasional:</label>
+                    <select class="form-select" id="jenis_kendaraan" name="jenis_kendaraan" required>
+                        <option value="">Pilih Jenis Kendaraan</option>
+                        <option value="Mobil Wuling">Mobil Wuling</option>
+                        <option value="Motor Vario">Motor Vario</option>
+                    </select>
                     <div class="invalid-feedback">
-                        Tanggal pemakaian harus diisi.
+                        Pilih jenis kendaraan operasional.
                     </div>
                 </div>
                 <div class="form-group mb-3">
-                    <label for="km">KM Pemakaian:</label>
-                    <input type="number" id="km" name="km" class="form-control" required>
+                    <label for="jenis_peminjaman">Jenis Peminjaman:</label>
+                    <select class="form-select" id="jenis_peminjaman" name="jenis_peminjaman" required>
+                        <option value="Tugas Lembaga">Tugas Lembaga</option>
+                        <option value="Keperluan Pribadi">Keperluan Pribadi</option>
+                    </select>
                     <div class="invalid-feedback">
-                        KM pemakaian harus diisi.
+                        Pilih jenis peminjaman.
                     </div>
                 </div>
-                <h3 class="mb-3">Tanda Tangan Pemakai</h3>
+                <div class="form-group mb-3">
+                    <label for="tanggal_peminjaman">Tanggal Peminjaman:</label>
+                    <input type="date" id="tanggal_peminjaman" name="tanggal_peminjaman" class="form-control" required>
+                    <div class="invalid-feedback">
+                        Tanggal peminjaman harus diisi.
+                    </div>
+                </div>
+                <div class="form-group mb-3">
+                    <label for="tanggal_pengembalian">Tanggal Pengembalian:</label>
+                    <input type="date" id="tanggal_pengembalian" name="tanggal_pengembalian" class="form-control" required>
+                    <div class="invalid-feedback">
+                        Tanggal pengembalian harus diisi.
+                    </div>
+                </div>
+                <div class="form-group mb-3">
+                    <label for="km_awal">Kilo Meter Awal Saat Peminjaman:</label>
+                    <input type="number" id="km_awal" name="km_awal" class="form-control" required>
+                    <div class="invalid-feedback">
+                        KM awal saat peminjaman harus diisi.
+                    </div>
+                </div>
+                <div class="form-group mb-3">
+                    <label for="lokasi_tujuan">Lokasi Tujuan:</label>
+                    <input type="text" id="lokasi_tujuan" name="lokasi_tujuan" class="form-control" placeholder="bandung, cikarang" required>
+                    <div class="invalid-feedback">
+                        Lokasi tujuan harus diisi.
+                    </div>
+                </div>
+                <h3 class="mb-3">Tanda Tangan Peminjam</h3>
                 <canvas id="signature-pad" class="mb-3"></canvas>
                 <button type="button" id="clear" class="btn btn-warning mb-3">Clear Tanda Tangan</button>
 
